72782872bbafab816b668d8192ed9bf4a463f063
dalmook <94118801+dalmook@users.noreply.github.com>
2025-09-01 22:52:32 +0900

    Create vocab.js

---
+++ vocab.js
@@ -0,0 +1,20 @@
+[
+  {
+    "hanzi": "学习",
+    "pinyin": "xuéxí",
+    "meaning": "공부하다",
+    "pos": "동사",
+    "chapter": "chapter1",
+    "example": "我们一起学习中文。",
+    "example_ko": "우리 함께 중국어를 공부해요."
+  },
+  {
+    "hanzi": "朋友",
+    "pinyin": "péngyou",
+    "meaning": "친구",
+    "pos": "명사",
+    "chapter": "chapter1",
+    "example": "他是我的好朋友。",
+    "example_ko": "그는 내 친한 친구이다."
+  }
+]
